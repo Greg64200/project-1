2265c0868fb8b193f4230c22491db9ed8a0e558c
Guillaume40480 <jumangi357@yahoo.fr>
2021-10-07 22:30:47 +0200

    added video and header on all pages

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ document.addEventListener("submit", function (event) {
   event.preventDefault();
   alert(message);
 });
+const navBar = document.querySelector(".nav_bar");
+
+window.addEventListener("wheel", (e) => {
+    if(e.deltaY >0){
+        navBar.style.transform = "translateY(-35vh)";
+    } else {
+        navBar.style.transform = "translateY(0)";
+    }
+});
